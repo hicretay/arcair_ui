dd150ac391ce5dd33bfa55e6f20db424ffab7411
Hicret Ay <46728765+hicrett@users.noreply.github.com>
2021-06-22 23:32:04 +0300

    220621

--- arcair/lib/screens/homePage.dart
+++ arcair/lib/screens/homePage.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:arcair/models/dataModel.dart';
 import 'package:arcair/settings/consts.dart';
+import 'package:arcair/settings/functions.dart';
 import 'package:arcair/widgets/backgroundWidget.dart';
 import 'package:arcair/widgets/deviceCardWidget.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,32 +14,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  dynamic _temperatureJson = " ";
-  dynamic _humidityJson = " ";
-
   // ---DeviceCardWidget sınıfı türünde cihaz bilgileri listesi----
   //List<DeviceCardWidget> devices = [];
   //------------------------------------------------------------
-  @override
-  void initState() {
-    super.initState();
-    setState(() {
-      weatherData();
-    });
-  }
+  Weather _weatherdata;
 
-  Future<void> weatherData() async {
-    Response response = await get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      Map incomingJson = jsonDecode(response.body);
-      _temperatureJson = incomingJson["main"]["temp"];
-      _temperatureJson = (_temperatureJson - 273).toInt();
-      _humidityJson = incomingJson["main"]["humidity"];
-    } else {
-      _temperatureJson = "0";
-      _humidityJson = "0";
-    }
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -55,10 +37,7 @@ class _HomePageState extends State<HomePage> {
           size: 28,
         ),
         onPressed: () {
-          //Navigator.pushNamed(context, "/nearbyWifiPage");
-          setState(() {
-            weatherData();
-          });
+          Navigator.pushNamed(context, "/nearbyWifiPage");
         },
       ),
       //--------------------Sayfa Arkaplanı -----------------------
@@ -68,15 +47,34 @@ class _HomePageState extends State<HomePage> {
           child: ListView.builder(
               itemCount: 1,
               itemBuilder: (BuildContext context, int index) {
-                return DeviceCardWidget(
-                  airQuality: "43",
-                  cardColor: Color.fromRGBO(0, 255, 0, 0.2),
-                  deviceName: "AFYON",
-                  coLevel: "35",
-                  humidityLevel: _humidityJson.toString(),
-                  temperature: _temperatureJson.toString(),
-                  warningColor: Colors.transparent,
-                ); // devices listesi döndürülecek
+                return Column(
+                  children: [
+                    FutureBuilder(
+                      future: weatherDataFunc(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.data != null) {
+                          this._weatherdata = snapshot.data;
+                          if (this._weatherdata == null) {
+                            print("Birseyler yanlis gitti");
+                            return Text("Birseyler yanlis gitti");
+                          } else {
+                            return DeviceCardWidget(
+                              airQuality: "43",
+                              cardColor: Color.fromRGBO(0, 255, 0, 0.2),
+                              deviceName: "AFYON",
+                              coLevel: "35",
+                              humidityLevel: _weatherdata.humidity.toString(),
+                              temperature: _weatherdata.temp.toString(),
+                              warningColor: Colors.transparent,
+                            );
+                          } // devices listesi döndürülecek
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
+                  ],
+                );
               }),
         ),
       ),
--- arcair/lib/settings/functions.dart
+++ arcair/lib/settings/functions.dart
@@ -1,24 +1,20 @@
-// import 'dart:convert';
-// import 'package:arcair/settings/consts.dart';
-// import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:arcair/models/dataModel.dart';
+import 'package:arcair/settings/consts.dart';
+import 'package:http/http.dart' as http;
+
+ Future weatherDataFunc() async {
+    Weather weather;
+    final response = await http.post(
+      Uri.parse(url),
+    );
+    if (response.statusCode == 200) {
+      weather = Weather.fromJson(json.decode(response.body));
+    } else {
+      return null;
+    }
+    return weather;
+  }
 
 
-//-------------------Login Fonksiyonu-----------------------------------
-// class LoginService {
-//   Future<LoginModel> loginUser(String email, String password) async {
-//     final response = await http.post(
-//       Uri.parse("https://reqres.in/api/login"),
-      
-//       body: {"email": email, "password": password},
-//       headers: {'Content-type': 'application/json'},
-//     );
 
-//     if (response.statusCode == 200) {
-//       final String responseString = response.body;
-//       return loginModelFromJson(responseString);
-//     } else {
-//       return null;
-//     }
-//   }
-// }
-//---------------------------------------------------------------------
